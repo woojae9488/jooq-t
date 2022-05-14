d7d9f76da239b730157cb4de7b006384d44a9aef
woojae9488 <woojae9488@naver.com>
2022-05-15 07:06:47 +0900

    Init project

---
+++ src/main/java/com/kwj/jooqt/JooqTApplication.java
@@ -0,0 +1,13 @@
+package com.kwj.jooqt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JooqTApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JooqTApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kwj/jooqt/JooqTApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kwj.jooqt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JooqTApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
